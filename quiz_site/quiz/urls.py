26469b00a5e214b0c1aad3f8a90dc4bdc6b8b8bf
torohkiss <torohkiss@yahoo.com>
2025-01-20 14:37:11 +0000

    Updated thsult template file and app views module to render correcrect results

--- quiz_site/quiz/urls.py
+++ quiz_site/quiz/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
         path('', views.all, name='all_questions'),
-        path('question/<int:question_id>/', views.question_view, name='question'),
+        #path('question/<int:question_id>/', views.question_view, name='question'),
         path('submit/', views.submit_answers, name='submit_answers'),
     ]
--- quiz_site/quiz/views.py
+++ quiz_site/quiz/views.py
@@ -5,8 +5,22 @@ from .forms import QuestionForm
 # Create your views here.
 
 def all(request):
-    #questions = Question.objects.all()
-    questions = Question.objects.prefetch_related('choices').all()
+    questions = Question.objects.all()
+    #questions = Question.objects.prefetch_related('choices').all()
+    results = []
+
+    if request.method == "POST":
+        for question in questions:
+            choice_id = request.POST.get(f'question_{question.id}')
+            if choice_id:
+                choice = Choice.objects.get(id=choice_id)
+                is_correct = choice.correct_answer
+                results.append({
+                    'question': question.text,
+                    'selected_choice': choice.text,
+                    'is_correct': is_correct,
+                })
+        return render(request, 'quiz/result.html', {'results': results})
     return render(
             request,
             'quiz/all_questions.html',
@@ -17,20 +31,22 @@ def all(request):
 
 def submit_answers(request):
     if request.method == 'POST':
-        results = {}
+        results = []
         for question in Question.objects.all():
             selected_choice_id = request.POST.get(f'question_{question.id}')
+
             if selected_choice_id:
                  selected_choice = Choice.objects.get(id=selected_choice_id)
-                 results[question.id] = {
+
+                 results.append({
                     'question': question.text,
-                    'selected': selected_choice.text,
-                    'correct_answer': selected_choice.correct_answer
-                }
-        return render(request, 'quiz/results.html', {'results': results})
+                    'selected_choice': selected_choice.text,
+                    'is_correct': selected_choice.correct_answer
+                })
+        return render(request, 'quiz/result.html', {'results': results})
     return redirect('all_questions')
 
-def question_view(request, question_id):
+"""def question_view(request, question_id):
     question = get_object_or_404(Question, id=question_id)
 
     if request.method == 'POST':
@@ -46,4 +62,4 @@ def question_view(request, question_id):
     else:
         form = QuestionForm(question=question)
 
-    return render(request, 'quiz/question.html', {'form': form, 'question': question})
+    return render(request, 'quiz/question.html', {'form': form, 'question': question})"""
